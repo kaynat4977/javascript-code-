ee47c76db88af9364aa7f7dcb07088df943e6b02
Kaynat Shabbir <shabbirkainat75@gmail.com>
2024-11-22 19:55:24 +0500

    Add files via upload

---
+++ indexfile.js
@@ -0,0 +1,79 @@
+
+   // let b =20;
+   // console.log("RESULT");
+    //console.log("The value of b is " , b);
+   // console.log(".....................");
+   // b++;
+   // console.log("The value of a++ is",b);
+   // console.log("Now the value of b is",b);
+    
+   // console.log("The value of b is " , b);
+   // b++;
+   // console.log("Now the value of b is",b);
+   // --b;
+    //console.log("The value of --b is " , b);
+    //console.log("now the value of b is",b);
+    
+    //console.log("The value of b-- is",b);
+    //b--;
+    //console.log("Now the value of b-- is",b);
+
+    //QUESTION NO 2//
+    //let a=30;
+    //let b=20;
+    //let answer = --a - --b + ++b +b--;
+    //--a;
+    //--b;
+    //++b;
+   // b--;
+    //console.log("a");
+    //console.log("b");
+    //console.log("answer");
+    
+    //QUESTION NO 3//
+    //let Name=prompt("ENTER YOUR CAMPUS NAME");
+    //alert("welcome to saylani mass it training");
+
+    //QUESTION NO 4//
+    //let user=prompt("ENTER A NUMBER");
+    //for(let k=5;k<=20;k++){
+     // console.log(user , "X" , k , "=" , user*k);
+    //}
+
+   // Question no 5//
+  // let english=prompt("enter your english marks");
+  // let physics=prompt("enter your physics number");
+  // let computer=prompt("enter your computer number");
+  // let totalnumber=prompt("/t/t/t/t/t enter total number");
+  // console.log("SUBJECT TOTALNUMBER TOTAL MARKSOBTAIN PERCENTAGE ");
+  // let obtainedmarks1=parseInt(prompt("enter your marks obtained"));
+   //let obtainedmarks2=parseInt(prompt("enter your marks obtained"));
+  // let obtainedmarks3=parseInt(prompt("enter your marks obtained"));
+  // console.log(sub1 , "     " +totalnumber , "       " , "        " ,obtainedmarks1 , "      " ,obtainedmarks1/totalnumber*100 + "%" );
+  // console.log(sub2 , "     " +totalnumber , "       " , "        " ,obtainedmarks1 , "      " ,obtainedmarks1/totalnumber*100 + "%" );
+  // console.log(sub3 , "     " +totalnumber , "       " , "        " ,obtainedmarks1 , "      " ,obtainedmarks1/totalnumber*100 + "%" );
+  // let sum=totalnumber*3;
+  // let totalobtained=obtainedmarks1+obtainedmarks2+obtainedmarks3;
+  // let totalPERCENTAGE = totalobtained/sum*100;
+  // console.log("      " +sum+ "   " ,"+ totalobtained", "totalpercentage+%");
+
+  
+   
+
+
+
+   
+
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
